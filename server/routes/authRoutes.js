9bb4de6e9c2b469e8c9daf23ce25f2ea09a95e8b
bee_bek_wagle <128395029+Bibek1619@users.noreply.github.com>
2025-08-16 11:15:58 +0545

    l

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,7 +5,7 @@ const { registerUser, loginUser, getUserProfile } = require('../controllers/auth
 const { protect } = require('../middlewares/authMiddleware');
 const upload = require('../middlewares/upload'); 
 
-// Routes
+
 router.post('/register', upload.single("shopLogo"), registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile);
